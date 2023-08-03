4a2f4e8f6b403493e900009b179efe12040d1eec
Vitorhenriquesilvadesa <vitorhenriquesilvadesa@gmail.com>
2023-08-03 00:12:20 -0300

    adding

--- src/org/ignite/renderer/buffers/Buffer.java
+++ src/org/ignite/renderer/buffers/Buffer.java
@@ -3,7 +3,7 @@ package org.ignite.renderer.buffers;
 public final class Buffer {
 
     public static VertexBuffer createVertexBuffer(float[] vertices) {
-
+        
     }
 
     public static IndexBuffer createIndexBuffer(int[] indices) {
---
+++ src/org/ignite/renderer/buffers/glbuffers/GLVertexBuffer.java
@@ -0,0 +1,7 @@
+package org.ignite.renderer.buffers.glbuffers;
+
+import org.ignite.renderer.buffers.VertexBuffer;
+
+public class GLVertexBuffer extends VertexBuffer {
+
+}
